51f14e1606444edfc52c6b40986df36e8c4a267e
Sarvesh Muttepwar <muttepwarsarvesh3958@gmail.com>
2025-01-19 23:39:30 +0530

    This is react 3rd session

--- App.js
+++ App.js
@@ -1,28 +1,40 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement =>Object =>ReactDom=>Html
-// JSX =>React.createElement =>ReactElement=>js object =>HtmlElement(render)
+const Title = () => (
+    <h1 className="root" tabIndex="5"
+    >Namaste react using Jsx🚀</h1>
+);
 
-// const heading = React.createElement(
-//     "h1",
-//     { id: "heading" },
-//     "Namaste React Bhai 🚀"
-// );
-// console.log(heading);
-
-//How do we render it?
-//Through the root
-
-//Jsx - is not html in javascript
-//you can called it as html like syntax or xml like syntax
-const jsxHeading = (<h1 className="root" tabIndex="5"
->Namaste react using Jsx🚀</h1>);
+//How we can put react element in the component
+const Title_1 = (<h1 className="root" tabIndex="5"
+>Namaste react using Jsx🚀</h1>
+);
 
 
-console.log(jsxHeading);
+//React Components
+//Class based components-old war
+//Functional components-new way
+//React Functional component
+//Its a normal javascript function which returns some jsx(react) element.
+
+const number = 10000
+
+
+const HeadingComponent_1 = () => (
+    <div className="container">
+        <Title />
+        {/* Rendering functional react component */}
+        {Title()}
+        {/* console.log("Rendering functional react component");  */}
+        <h1>{number}</h1>
+        {/* rendering javascript */}
+        {Title_1}
+        {/* rendering react element */}
+        <h1 className="heading">Namaste React one liner or multiliner (For multi use brackets) Functional Components</h1>
+    </div>)
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(heading);
-root.render(jsxHeading);
+root.render(<HeadingComponent_1 />);
+
